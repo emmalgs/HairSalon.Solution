a70eb7a69b44ff6ab7d57f478eb7cd2f29ce7aa6
Emma Gerigscott <elgerig@gmail.com>
2023-05-19 16:39:56 -0700

    update home index to display appointment count

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
 using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly HairSalonContext _db;
+    public HomeController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
+      List<Appointment> allApts = _db.Appointments.ToList();
+      ViewBag.AllAppointments = allApts;
       return View();
     }
   }
