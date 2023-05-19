8895a1dcaa3dcb93a61003424ec4a55bce4a49f9
Emma Gerigscott <elgerig@gmail.com>
2023-05-19 13:33:13 -0700

    fix data types for date and time

--- HairSalon/Models/Appointment.cs
+++ HairSalon/Models/Appointment.cs
@@ -5,8 +5,8 @@ namespace HairSalon.Models
   public class Appointment
   {
     public int AppointmentId { get; set; }
-    public DateOnly Date { get; set; }
-    public TimeOnly Time { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan Time { get; set; }
     public int ClientId { get; set; }
     public int StylistId { get; set; }
     public Stylist Stylist { get; set; }
