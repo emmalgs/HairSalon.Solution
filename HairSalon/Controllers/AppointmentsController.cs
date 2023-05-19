c7e3557b70da9f806e5b7de6a5fd2602b75e9e12
Emma Gerigscott <elgerig@gmail.com>
2023-05-19 16:00:47 -0700

    add delete page and functionality for appointments

--- HairSalon/Controllers/AppointmentsController.cs
+++ HairSalon/Controllers/AppointmentsController.cs
@@ -74,6 +74,23 @@ namespace HairSalon.Controllers
       _db.Appointments.Update(appointment);
       _db.SaveChanges();
       return RedirectToAction("Index");
-    } 
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Appointment foundApt = _db.Appointments.Include(model => model.Client)
+                                      .Include(model => model.Stylist)
+                                      .FirstOrDefault(model => model.AppointmentId == id);
+      return View(foundApt);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Appointment foundApt = _db.Appointments.FirstOrDefault(model => model.AppointmentId == id);
+      _db.Remove(foundApt);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
