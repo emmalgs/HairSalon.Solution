7d3aa2c8cf5fdcaaab47dff6c809303b5dae6c32
Emma Gerigscott <elgerig@gmail.com>
2023-05-19 09:41:56 -0700

    create index page for clients, add stylist list length check

---
+++ HairSalon/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> allStylists = _db.Stylists.ToList();
+      ViewBag.AllStylistsList = allStylists;
+
+      List<Client> allClients = _db.Clients.ToList();
+      return View(allClients);
+    }
+  }
+}
